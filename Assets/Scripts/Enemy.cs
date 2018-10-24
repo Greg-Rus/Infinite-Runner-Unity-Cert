ae46232b2f9f2f60b738fd9f5252beb6ca485c6a
Greg-Rus <grzegorz.rus@gmail.com>
2018-10-24 23:16:53 +0200

    Enemy script

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal.VR;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    public int HitPoints = 1;
+    public string PlayerTag = "Player";
+
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void TakeDamage()
+    {
+        HitPoints--;
+        if (HitPoints <= 0)
+        {
+            EnemyDied();
+        }
+    }
+
+    private void EnemyDied()
+    {
+        Destroy(gameObject);
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log("Collided with: " + col.collider.tag);
+        if(col.collider.CompareTag(PlayerTag)) TakeDamage();
+    }
+}
