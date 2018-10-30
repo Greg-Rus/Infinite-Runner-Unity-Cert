d99d770fea3ba28f44db4cf8edb488fa0bf8575c
Greg-Rus <grzegorz.rus@gmail.com>
2018-10-30 23:30:57 +0100

    Minimap tracking and tags

---
+++ Assets/Scripts/MiniMapMover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapMover : MonoBehaviour
+{
+    public Transform Player;
+
+    public float MinimapLead;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    var newMiniMapCameraPosition = Player.position;
+	    newMiniMapCameraPosition.x += MinimapLead;
+	    newMiniMapCameraPosition.z = -10;
+	    transform.position = newMiniMapCameraPosition;
+	}
+}
---
+++ Assets/Scripts/TagController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class TagController : MonoBehaviour
+{
+    private Transform _tagTransform;
+    private Transform _parent;
+    private Camera _minimapCamera;
+	// Use this for initialization
+	void Start ()
+	{
+	    _tagTransform = transform;
+	    _minimapCamera = FindObjectsOfType<Camera>().SingleOrDefault(camera => camera.CompareTag("MiniMapCamera"));
+	    _parent = _tagTransform.parent.transform;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+	    _tagTransform.transform.rotation = Quaternion.identity;
+	    Vector3[] frustumCorners = new Vector3[4];
+	    _minimapCamera.CalculateFrustumCorners(new Rect(0, 0, 1, 1), _minimapCamera.farClipPlane, Camera.MonoOrStereoscopicEye.Mono, frustumCorners);
+	    Vector3[] cornersInWorldSpace =
+	        frustumCorners.Select(corner => _minimapCamera.transform.TransformVector(corner) + _minimapCamera.transform.position).ToArray();
+	    var rightScreenEdge = cornersInWorldSpace.Max(edge => edge.x);
+	    var leftScreenEdge = cornersInWorldSpace.Min(edge => edge.x);
+	    if (_parent.position.x > rightScreenEdge)
+	    {
+	        MoveTagToEdge(rightScreenEdge);
+
+	    }
+        else if (_parent.position.x < leftScreenEdge)
+	    {
+	        MoveTagToEdge(leftScreenEdge);
+	    }
+	    else
+	    {
+	        _tagTransform.position = _parent.position;
+	    }
+
+	}
+
+    private void MoveTagToEdge(float edgePosition)
+    {
+        var newPosition = _parent.position;
+        newPosition.x = edgePosition;
+        _tagTransform.position = newPosition;
+    }
+}
